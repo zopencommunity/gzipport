2f8351e6e9b621d57df7a6d95a65919c49a34114
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:25 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/gzipport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/gzipport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/gzipport.git'), string(name: 'PORT_DESCRIPTION', value: 'gzip is a file format and a software application used for file compression and decompression.' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/gzipport.git'), string(name: 'PORT_DESCRIPTION', value: 'gzip is a file format and a software application used for file compression and decompression.' )]
         }
 }
